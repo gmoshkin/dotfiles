4f8075698798cdc51984f1962dbcb955f4ac9807
gmoshkin <louielouie314@gmail.com>
2016-12-20 15:18:18 +0300

    Update crontab task for updating ctrlp cache

--- ctrlp_cache.sh
+++ ctrlp_cache.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 source ~/dotfiles/utils.sh
 
-cd ~/.vim
-find_all > ~/.cache/ctrlp/%home%mgn%.vim.txt
+USER="$(whoami)"
+
+VIMFILES=${VIMFILES:-"/home/${USER}/.vim"}
+if [ -d "$VIMFILES" ]; then
+    cd "$VIMFILES"
+    find_all > ~/.cache/ctrlp/%home%${USER}%.vim.txt
+fi
+
+DOTFILES=${DOTFILES:-"/home/${USER}/dotfiles"}
+if [ -d "$DOTFILES" ]; then
+    cd "$DOTFILES"
+    find_all > ~/.cache/ctrlp/%home%${USER}%dotfiles.txt
+fi
+
+REP_DIR=${REP_DIR:-"/space/${USER}/REP"}
+if [ -d "$REP_DIR" ]; then
+    cd "$REP_DIR"
+    find_all > ~/.cache/ctrlp/%space%${USER}%REP.txt
+fi
