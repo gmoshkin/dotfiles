561592994c5fd3b93f67b23bb64c78072534ed8b
gmoshkin <louielouie314@gmail.com>
2017-01-25 15:42:00 +0300

    Fix some colors in colors.sh

--- colors.sh
+++ colors.sh
@@ -26,7 +26,7 @@ CLRFGBASE1='[96m'
 CLRFGBASE2='[37m'
 CLRFGBASE3='[97m'
 
-CLRBGBASE04='[100m'
+CLRBGBASE03='[100m'
 CLRBGBASE02='[40m'
 CLRBGBASE01='[102m'
 CLRBGBASE00='[104m'
@@ -50,8 +50,8 @@ CLRBGYELLOW='[43m'
 CLRBGBLUE='[44m'
 CLRBGMAGENTA='[45m'
 CLRBGCYAN='[46m'
-CLRBGORANGE='[101'
-CLRBGVIOLET='[105'
+CLRBGORANGE='[101m'
+CLRBGVIOLET='[105m'
 
 CLRRESET='[00m'
 CLRBOLD='[1m'
@@ -74,13 +74,13 @@ function show_color {
 
 function show_colors {
     show_color red     ${CLRFGRED}     invert
-    show_color green   ${CLRFGGREEN}   invert
-    show_color yellow  ${CLRFGYELLOW}  invert
-    show_color blue    ${CLRFGBLUE}    invert
-    show_color magenta ${CLRFGMAGENTA} invert
-    show_color cyan    ${CLRFGCYAN}    invert
     show_color orange  ${CLRFGORANGE}  invert
+    show_color yellow  ${CLRFGYELLOW}  invert
+    show_color green   ${CLRFGGREEN}   invert
+    show_color cyan    ${CLRFGCYAN}    invert
+    show_color blue    ${CLRFGBLUE}    invert
     show_color violet  ${CLRFGVIOLET}  invert
+    show_color magenta ${CLRFGMAGENTA} invert
     show_color base03  ${CLRFGBASE03}  invert
     show_color base02  ${CLRFGBASE02}  invert
     show_color base01  ${CLRFGBASE01}  invert
@@ -90,14 +90,31 @@ function show_colors {
     show_color base2   ${CLRFGBASE2}   invert
     show_color base3   ${CLRFGBASE3}   invert
 
+    # show_color red     ${CLRBGRED}
+    # show_color orange  ${CLRBGORANGE}
+    # show_color yellow  ${CLRBGYELLOW}
+    # show_color green   ${CLRBGGREEN}
+    # show_color cyan    ${CLRBGCYAN}
+    # show_color blue    ${CLRBGBLUE}
+    # show_color violet  ${CLRBGVIOLET}
+    # show_color magenta ${CLRBGMAGENTA}
+    # show_color base03  ${CLRBGBASE03}
+    # show_color base02  ${CLRBGBASE02}
+    # show_color base01  ${CLRBGBASE01}
+    # show_color base00  ${CLRBGBASE00}
+    # show_color base0   ${CLRBGBASE0}
+    # show_color base1   ${CLRBGBASE1}
+    # show_color base2   ${CLRBGBASE2}
+    # show_color base3   ${CLRBGBASE3}
+
     show_color red     ${CLRFGRED}
-    show_color green   ${CLRFGGREEN}
-    show_color yellow  ${CLRFGYELLOW}
-    show_color blue    ${CLRFGBLUE}
-    show_color magenta ${CLRFGMAGENTA}
-    show_color cyan    ${CLRFGCYAN}
     show_color orange  ${CLRFGORANGE}
+    show_color yellow  ${CLRFGYELLOW}
+    show_color green   ${CLRFGGREEN}
+    show_color cyan    ${CLRFGCYAN}
+    show_color blue    ${CLRFGBLUE}
     show_color violet  ${CLRFGVIOLET}
+    show_color magenta ${CLRFGMAGENTA}
     show_color base03  ${CLRFGBASE03}
     show_color base02  ${CLRFGBASE02}
     show_color base01  ${CLRFGBASE01}
