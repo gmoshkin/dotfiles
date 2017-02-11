d1ca5c2d5bcbe03a4ff683597621c43edce9309c
mgn <louielouie314@gmail.com>
2017-02-11 15:48:35 +0300

    Removed all of the deprecated deploy scripts

--- deploy_gdbinit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir="$(dirname $0)"
-path="$(readlink -f $dir)"
-ln -s "$path/gdbinit" ~/.gdbinit
--- deploy_gtkrc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir="$(dirname $0)"
-path="$(readlink -f $dir)"
-ln -s "$path/gtkrc-2.0" ~/.gtkrc-2.0
