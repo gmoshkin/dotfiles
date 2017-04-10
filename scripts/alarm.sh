c4f5ff9c86464dd004b461accd9a8e762678b4b3
mgn <louielouie314@gmail.com>
2017-04-10 16:26:45 +0300

    Update the alarm script to play local music

--- scripts/alarm.sh
+++ scripts/alarm.sh
@@ -9,18 +9,26 @@ STREAMING_ADDRESS=http://eradioportal.com/monsterRx.asx
 STREAMING_ADDRESS=http://23.92.61.218:80
 
 # Mute the speakers.  You don't want to be rudely awakened
-# by a sudden rush of sound from the speakers now, do you? 
+# by a sudden rush of sound from the speakers now, do you?
 amixer -q set Master 0
 
 # Let's use VLC to stream content.
-DISPLAY=:0 cvlc $STREAMING_ADDRESS &
+case "$1" in
+    "local" )
+        cd /media/gmoshkin/56BE0E36BE0E0EE5/Users/mgn/Music
+        DISPLAY=:0 mpg321 -@ shuffled -Z &
+        ;;
+    "radio" | * )
+        DISPLAY=:0 cvlc $STREAMING_ADDRESS &
+        ;;
+esac
 # sleep 10  # A 10-second wait should be enough time to fill up the buffer.
 
 # Gradually turn the volume up in 5% increments every 2 seconds
 for STEP in `seq 0 2 50`
 do
-  amixer -q set Master $STEP%
-  sleep 1
+    amixer -q set Master $STEP%
+    sleep 1
 done
 
 # EOF
