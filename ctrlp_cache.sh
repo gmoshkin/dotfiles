1b42f4232303540e476bef22361d68d7e91589e5
gmoshkin <louielouie314@gmail.com>
2017-06-23 16:38:31 +0300

    Add PRs directory to the ctrlp cache list

--- ctrlp_cache.sh
+++ ctrlp_cache.sh
@@ -8,6 +8,7 @@ VIMFILES=${VIMFILES:-"/home/${USER}/.vim"}
 DOTFILES=${DOTFILES:-"/home/${USER}/dotfiles"}
 REP_DIR=${REP_DIR:-"/space/${USER}/REP"}
 CSPROJ=${CSPROJ:-"/space/${USER}/projects/csharp"}
+PRS=${PRS:-"/space/${USER}/PRs"}
 
 declare -A CACHE
 CACHE=(
@@ -15,13 +16,17 @@ CACHE=(
     ["$DOTFILES"]="$CTRLPCACHE/%home%${USER}%dotfiles.txt"
     ["$REP_DIR"]="$CTRLPCACHE/%space%${USER}%REP.txt"
     ["$CSPROJ"]="$CTRLPCACHE/%space%${USER}%projects%csharp.txt"
+    ["$PRS"]="$CTRLPCACHE/%space%${USER}%PRs.txt"
 )
 
+# in order to update the cache for just one directory, comment out the rest of
+# them in this array
 DIRS=(
     "$VIMFILES"
     "$DOTFILES"
     "$REP_DIR"
     "$CSPROJ"
+    "$PRS"
 )
 
 for d in ${DIRS[@]}; do
