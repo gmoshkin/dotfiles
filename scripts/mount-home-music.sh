9c55c21ab6a61e920a6d41ca6f966f3b5c2f2e21
gmoshkin <louielouie314@gmail.com>
2017-10-04 15:18:07 +0300

    Fix the script to mount the home directory with music locally

--- scripts/mount-home-music.sh
+++ scripts/mount-home-music.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshfs -p $HOME_PORT -o allow_other -o IdentityFile=~/.ssh/id_rsa $HOME_USER@$HOME_HOST:$HOME_MUSIC_PATH $LOCAL_MUSIC_PATH
+sudo sshfs -p $HOME_PORT -o allow_other -o IdentityFile=~/.ssh/id_rsa $HOME_USER@$HOME_HOST:$HOME_MUSIC_PATH $LOCAL_MUSIC_PATH
