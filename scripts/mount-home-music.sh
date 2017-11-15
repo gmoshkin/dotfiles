696406e509286a31643a669a13bfe123edf3e25a
Georgy Moshkin <louielouie314@gmail.com>
2017-11-15 14:08:41 +0300

    Update mount-home-music script

--- scripts/mount-home-music.sh
+++ scripts/mount-home-music.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+config=~/.home-music-conf
+if [ ! -f $config ]; then
+    echo $config doesn\'t exist, please create it
+    exit 1
+fi
+
+source $config
+
+if [ -z "$HOME_PORT" -o -z "$HOME_USER" -o -z "$HOME_HOST" -o -z "$HOME_MUSIC_PATH" \
+    -o -z "$LOCAL_MUSIC_PATH" ]; then
+    echo variables HOME_PORT HOME_USER HOME_HOST HOME_MUSIC_PATH LOCAL_MUSIC_PATH \
+        must be set
+    exit 2
+fi
+
 sudo sshfs -p $HOME_PORT -o allow_other -o IdentityFile=~/.ssh/id_rsa $HOME_USER@$HOME_HOST:$HOME_MUSIC_PATH $LOCAL_MUSIC_PATH
