19f2241a523c61518276dae09044466c749de6f1
Georgy Moshkin <louielouie314@gmail.com>
2017-11-30 10:57:42 +0300

    Enable 'new' configuration for tmux master version

--- scripts/tmuxversion.sh
+++ scripts/tmuxversion.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-  exit -1
+    exit -1
 fi
 
 needed="$1"
 version=$(tmux -V | cut -d' ' -f2)
 # the result of the script would be true if version >= needed
-exit $(echo "${version}<${needed}" | bc)
+if [ "${version}" = master ]; then
+    exit 0
+else
+    exit $(echo "${version}<${needed}" | bc)
+fi
