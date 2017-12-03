e267103361f5a721ead5dc90ce16cb7f1d40238e
mgn <louielouie314@gmail.com>
2017-12-03 20:05:03 +0300

    Start the music session with the whole layout ready

--- scripts/start-cmus.sh
+++ scripts/start-cmus.sh
@@ -4,4 +4,13 @@ options=
 if [ -n "$CMUS_ADDR" ]; then
     options+="--listen $CMUS_ADDR"
 fi
+pane_height=$(tmux display -p '#{pane_height}')
+if [ ! -f /tmp/cover.png ]; then
+    if [ -f ~/Pictures/sad.png ]; then
+        cp ~/Pictures/sad.png /tmp/cover.png
+    fi
+fi
+tmux split-window -h -l $((pane_height * 2)) ~/dotfiles/scripts/screensaver.py i /tmp/cover.png
+tmux split-window -v -t .0 ~/cava/cava
+tmux select-pane -t .0
 eval "cmus $options"
