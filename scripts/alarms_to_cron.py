3f1aab46f25ff6a18c849aee4225f1a604b131d1
mgn <louielouie314@gmail.com>
2018-03-23 21:13:29 +0300

    Switch to PyYaml implementation

--- scripts/alarms_to_cron.py
+++ scripts/alarms_to_cron.py
@@ -9,7 +9,7 @@ preamble = 'SHELL=/bin/bash'
 command = '/usr/sbin/etherwake 9C:5C:8E:84:B6:33'
 
 with open(os.path.expanduser('~/.config/alarms.yaml')) as f:
-    days = yaml.load(f, Loader=yaml.RoundTripLoader)
+    days = yaml.load(f)
 
 dow_map = {
     'mon' : 1,
@@ -24,17 +24,27 @@ dow_map = {
     'weeke' : '6,7',
 }
 _5min = datetime.timedelta(seconds=5*60)
-print(preamble)
-for k, vs in days.items():
+
+def prepk(k):
     k = k.lower()
     if k[:4] == 'week':
-        k = k[:4]
+        k = k[:5]
     else:
         k = k[:3]
-    dow = dow_map[k]
+    return k
+
+def prepv(v):
+    if isinstance(v, int):
+        return '{}:{}'.format(v // 60, v % 60)
+    else:
+        return v
+
+print(preamble)
+for k, vs in days.items():
+    dow = dow_map[prepk(k)]
     if not isinstance(vs, list):
         vs = [vs]
     for v in vs:
-        time = datetime.datetime.strptime(v, '%H:%M')
+        time = datetime.datetime.strptime(prepv(v), '%H:%M')
         min_hour = (time - _5min).strftime('%M %H')
         print('{} * * {} {}'.format(min_hour, dow, command))
