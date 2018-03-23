2232bdca6c5784867411e94e4ce45a3a13fc3de1
gmoshkin <louielouie314@gmail.com>
2018-03-23 21:07:24 +0300

    Add support for weekdays/ends

--- scripts/alarms_to_cron.py
+++ scripts/alarms_to_cron.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from ruamel import yaml
+import yaml
 import os
 import datetime
 
@@ -19,12 +19,19 @@ dow_map = {
     'fri' : 5,
     'sat' : 6,
     'sun' : 7,
-    'all' : '*'
+    'all' : '*',
+    'weekd' : '1,2,3,4,5',
+    'weeke' : '6,7',
 }
 _5min = datetime.timedelta(seconds=5*60)
 print(preamble)
 for k, vs in days.items():
-    dow = dow_map[k.lower()[:3]]
+    k = k.lower()
+    if k[:4] == 'week':
+        k = k[:4]
+    else:
+        k = k[:3]
+    dow = dow_map[k]
     if not isinstance(vs, list):
         vs = [vs]
     for v in vs:
