eca3b2069fedcdc2cbe525b70ee6f3549f742da6
Georgy Moshkin <louielouie314@gmail.com>
2018-04-19 15:14:22 +0300

    Show color from command line argument

--- scripts/show_color.py
+++ scripts/show_color.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pymouse import PyMouse
-from pyscreenshot import grab
+import sys
+import re
 
 def rgb2hex(rgb):
     return '#{:06x}'.format(rgb[0] * 2**16 + rgb[1] * 2 ** 8  + rgb[2])
+
+def hex2rgb(hex):
+    m = re.match(r'#?([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})', hex,
+                 re.IGNORECASE)
+    return tuple(int(c, base=16) for c in m.groups())
 
 def show_color(rgb):
     r, g, b = rgb
@@ -13,4 +18,9 @@ def show_color(rgb):
                                                               hex=rgb2hex(rgb)))
 
 if __name__ == '__main__':
-    show_color(grab().getpixel(PyMouse().position()))
+    if len(sys.argv) > 1:
+        show_color(hex2rgb(sys.argv[1]))
+    else:
+        from pymouse import PyMouse
+        from pyscreenshot import grab
+        show_color(grab().getpixel(PyMouse().position()))
