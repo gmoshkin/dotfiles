a6b6401c4c3c4de43946c63bbe4a93959dc90326
gmoshkin <louielouie314@gmail.com>
2018-07-11 20:52:00 +0300

    Enable python3 in vim configuration

--- scripts/vim-configure.sh
+++ scripts/vim-configure.sh
@@ -7,15 +7,15 @@ FEATURES=(
 
 GUI="--enable-gui=yes --with-x=yes" # requires libsm-dev, libxt-dev, libx11-dev
 INTERPS=(
-    # "python3"
+    "python3"
     # "mzscheme"
-    "python" # requires libpython-dev
+    # "python" # requires libpython-dev
     "ruby" # requires ruby-dev
     "perl" # requires libperl-dev
     "lua" # ???
 )
 PYTHON2CONF="--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu"
-PYTHON3CONF="--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu"
+PYTHON3CONF="--with-python3-config-dir=/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu"
 
 ENABLEINTERPS=""
 for interp in ${INTERPS[@]}; do
@@ -32,4 +32,4 @@ LUAJIT="--with-luajit" # requires lib-luajit-5.1-dev
 PREFIX="--prefix=$HOME/.local"
 
 rm -f src/auto/config.cache
-./configure $PREFIX $FEATURE $ENABLEFEATURES $ENABLEINTERPS $GUI $PYTHON2CONF $LUAJIT
+./configure $PREFIX $FEATURE $ENABLEFEATURES $ENABLEINTERPS $GUI $PYTHON3CONF $LUAJIT
