4e3be67a1fc6f448a161bd4aba36a60144ce3bd8
gmoshkin <louielouie314@gmail.com>
2018-10-04 16:49:21 +0300

    even better

--- scripts/ps-tty.sh
+++ scripts/ps-tty.sh
@@ -6,7 +6,7 @@ TTY=${1}
 hl_executable() {
     sed 's/\(\\_\s\+\)\([^ ]\+\)/\1[35m\2[0m/'
 }
-hl_python_arg() {
-    sed 's/\(\\_\s\+\)\([^ ]*python[^ ]*\)\s\+\([^ ]\+\)/\1\2 [34m\3[0m/'
+hl_interp_arg() {
+    sed 's/\(\\_\s\+\)\([^ ]*\(python\|bash\)[^ ]*\)\s\+\([^ ]\+\)/\1\2 [34m\4[0m/'
 }
-ps f -t $TTY | hl_executable | hl_python_arg
+ps f -t $TTY | hl_executable | hl_interp_arg
