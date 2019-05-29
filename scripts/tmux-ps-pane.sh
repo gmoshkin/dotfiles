ba81b752f144e2379b290eb5f827a5bbf3f7ff1b
gmoshkin <louielouie314@gmail.com>
2019-05-29 19:29:04 +0300

    cool

--- scripts/tmux-ps-pane.sh
+++ scripts/tmux-ps-pane.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [ "$1" = -l ]; then
+    shift
+    out="less"
+fi
+
 pane=${1:-$(tmux display -p '#{pane_id}')}
 pane_id=$(tmux display -p -t "$pane" '#{pane_id}')
 pane_tty=$(tmux display -p -t $pane_id '#{pane_tty}')
 
-tmux split-window -t $pane_id "watch --color -n1 ps-tty.sh $pane_tty"
+if [ "$out" = "less" ]; then
+    tmux split-window -t $pane_id "ps-tty.sh $pane_tty | less"
+else
+    tmux split-window -t $pane_id "watch --color -n1 ps-tty.sh $pane_tty"
+fi
