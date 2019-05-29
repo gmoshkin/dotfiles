5f7af121e9f91f745fba2b97379442165a740ac0
gmoshkin <louielouie314@gmail.com>
2019-05-29 18:45:32 +0300

    ye

--- env.sh
+++ env.sh
@@ -11,4 +11,4 @@ export GOPATH="$HOME/gocode"
 export LESS=RMWSJ
 export SYSTEMD_LESS=RSMK
 export PS_FORMAT=pid,stat,tty,bsdstart,bsdtime,command
-export JQ_COLORS='0;36:0;31:0;32:0;34:1;35:0;33:0;35'
+export JQ_COLORS='36:31:32:34:95:33:35'
