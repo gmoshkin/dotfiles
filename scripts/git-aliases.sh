08814786d7bcb73a4c514dc1c46918cd0834b7e8
Georgy Moshkin <louielouie314@gmail.com>
2020-03-18 13:07:29 +0300

    im a bash master

--- scripts/git-aliases.sh
+++ scripts/git-aliases.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-
-for f in /usr/lib/git-core/*; do [ -x $f ] && [ ! -d $f ] &&  bf=$(basename $f); cmd=${bf#git-}; echo "alias g${cmd}='git ${cmd}'"; done
+for f in /usr/lib/git-core/*; {
+    [ -x $f ] && [ ! -d $f ] && {
+        bf=$(basename $f)
+        cmd=${bf#git-}
+        echo "alias g${cmd}='git ${cmd}'"
+    }
+}
