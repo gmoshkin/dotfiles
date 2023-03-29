8627574e1a3dd35ebfea05a8fc5ff51a28b16855
Georgy Moshkin <gmoshkin@picodata.io>
2023-03-29 10:50:13 +0300

    fzf: don't include hidden files

--- env.sh
+++ env.sh
@@ -21,7 +21,7 @@ export FZF_DEFAULT_OPTS="\
 "
 
 if type rg &>/dev/null; then
-    export FZF_DEFAULT_COMMAND='rg --files --hidden'
+    export FZF_DEFAULT_COMMAND='rg --files'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 
