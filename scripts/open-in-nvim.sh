0670b75e4d5c5b55e2a5abb498f33509ed725aca
Georgy Moshkin <gmoshkin@picodata.io>
2023-04-07 14:47:04 +0300

    open-in-nvim.sh: disable debug logging

--- scripts/open-in-nvim.sh
+++ scripts/open-in-nvim.sh
@@ -3,5 +3,5 @@
 filename=$(cat)
 cargo \
     --config build.target-dir=\"/tmp/target-nvim-cli\" \
-    run --manifest-path ~/dotfiles/scripts/nvim-cli/Cargo.toml &>>/tmp/open-in-nvim.log \
+    run --manifest-path ~/dotfiles/scripts/nvim-cli/Cargo.toml &>/dev/null \
     -- open "$filename"
