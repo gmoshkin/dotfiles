3b104a68c2c6a7a24bf7cd3a6784c0d2ffc83e51
Georgy Moshkin <gmoshkin@picodata.io>
2024-02-02 15:00:12 +0300

    tmux-ps-pane: use pageview instead of watch

--- scripts/tmux-ps-pane.sh
+++ scripts/tmux-ps-pane.sh
@@ -11,7 +11,7 @@ pane_tty=$(tmux display -p -t $pane_id '#{pane_tty}')
 case "$opt" in
     -l) cmd_fmt="%s | less" ;;
     -v) cmd_fmt="%s -C | vim -" ;;
-    *)  cmd_fmt="watch --color -n0.1 %s";;
+    *)  cmd_fmt="~/dotfiles/jai/pageview %s";;
 esac
 
 tmux split-window -t $pane_id "$(printf "$cmd_fmt" "ps-tty.sh $pane_tty -t")"
