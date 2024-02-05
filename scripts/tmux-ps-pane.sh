74f0cd03f4b4b38b96ae0ff55e6052f3a53f239f
Georgy Moshkin <gmoshkin@picodata.io>
2024-02-05 21:12:46 +0300

    ps.jai: 2ez4me

--- scripts/tmux-ps-pane.sh
+++ scripts/tmux-ps-pane.sh
@@ -11,7 +11,9 @@ pane_tty=$(tmux display -p -t $pane_id '#{pane_tty}')
 case "$opt" in
     -l) cmd_fmt="%s | less" ;;
     -v) cmd_fmt="%s -C | vim -" ;;
-    *)  cmd_fmt="~/dotfiles/jai/pageview %s";;
+    *)
+        exec tmux split-window -t $pane_id ~/dotfiles/jai/pageview -l /tmp/pageview.log ~/dotfiles/jai/ps -t $pane_tty
+        ;;
 esac
 
 tmux split-window -t $pane_id "$(printf "$cmd_fmt" "ps-tty.sh $pane_tty -t")"
