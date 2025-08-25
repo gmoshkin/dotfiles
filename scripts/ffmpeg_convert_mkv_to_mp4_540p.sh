68afe78cbffa4db20d85aee2335cafa196a2a6de
Georgy Moshkin <gmoshkin@picodata.io>
2025-08-26 00:49:54 +0300

    ffmpeg_convert_avi_to_mp4.sh

--- scripts/ffmpeg_convert_mkv_to_mp4_540p.sh
+++ scripts/ffmpeg_convert_mkv_to_mp4_540p.sh
@@ -1,1 +1,5 @@
-ffmpeg -i "$1" -vcodec libx265 -vf "scale=768:432" -map 0:v:0 -map 0:a:1 "${1%.mkv}.mp4"
+# `-ac 2` converts audio to stereo (it could originally be 6 channels or some shit like that)
+# `-map 0:v:0` use only the first video stream (start at 0)
+# `-map 0:a:1` use only the second audio stream (start at 0)
+# `-vf "scale=768:432"` rescale video to given resolution
+ffmpeg -i "$1" -vcodec libx265 -vf "scale=768:432" -map 0:v:0 -map 0:a:1 -ac 2 "${1%.mkv}.mp4"
